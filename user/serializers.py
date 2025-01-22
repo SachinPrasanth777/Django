57fbfbfd438a7598786edd3100c8fd8a82c4babc
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2025-01-22 20:51:58 +0530

    feat: enable roles

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Role, UserRole
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -9,16 +10,22 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class RegisterSerializer(serializers.ModelSerializer):
+    role = serializers.CharField(write_only=True, required=False)
+
     class Meta:
         model = User
-        fields = ("username", "email", "password")
+        fields = ("username", "email", "password", "role")
 
     def create(self, validated_data):
+        role_name = validated_data.pop("role", None)
         user = User.objects.create_user(
             validated_data["username"],
             validated_data["email"],
             validated_data["password"],
         )
+        if role_name:
+            role, created = Role.objects.get_or_create(name=role_name)
+            UserRole.objects.create(user=user, role=role)
         return user
 
 
